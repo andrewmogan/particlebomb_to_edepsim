260fcb64a6f276f9bbfd55c2bb48a2f7903ad108
Andrew Mogan <andrew.mogan@colostate.edu>
2021-07-21 11:57:16 -0700

    Refine class definition of ParticleBombKinematicsGenerator

--- particlebomb_to_edepsim/LinkDef.h
+++ particlebomb_to_edepsim/LinkDef.h
@@ -9,7 +9,6 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ class particlebomb_to_edepsim::sample+;
 #pragma link C++ class particlebomb_to_edepsim::test+;
 #pragma link C++ class particlebomb_to_edepsim::ParticleBombKinematicsFactory+;
 #pragma link C++ class particlebomb_to_edepsim::ParticleBombKinematicsGenerator+;
